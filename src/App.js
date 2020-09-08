f7ca436293de4c88632971fed3affb67fe2c34de
Andreea Furtuna <furtuna.andreeac@gmail.com>
2020-09-08 14:24:14 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This page is coded by Andreea Furtuna and is {""}
-        <a
-          href="https://github.com/Andreea-Furtuna/react-weather-app2"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This page is coded by Andreea Furtuna and is {""}
+          <a
+            href="https://github.com/Andreea-Furtuna/react-weather-app2"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
